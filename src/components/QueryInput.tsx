5d56785e64a50532f1e33df8a73e0974413e8214
Benjamin <shallifi@hotmail.com>
2024-02-24 14:06:55 -0500

    added button and p tag

--- src/components/QueryInput.tsx
+++ src/components/QueryInput.tsx
@@ -1,8 +1,14 @@
 
 
 const QueryInput = () => {
+
+  
   return (
+    <div>
+    <p className="text-gray-700">Enter your query below:</p>
     <input type="text" placeholder="Type here" className="input input-bordered w-full max-w-xs" />
+    <button className="btn btn-accent">Search</button>
+    </div>
   )
 }
 
