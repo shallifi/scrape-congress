a142851ef67dc4303c9ee89f249a46d8ad26581b
lempira <lempira@gmail.com>
2024-02-24 12:55:09 -0500

    Added vite react TS framework with daisyui and tailwindcss

---
+++ src/App.tsx
@@ -0,0 +1,13 @@
+import { useState } from "react";
+import "./App.css";
+import QueryInput from "./components/QueryInput";
+
+function App() {
+  return (
+    <>
+      <QueryInput />
+    </>
+  );
+}
+
+export default App;
---
+++ src/components/QueryInput.tsx
@@ -0,0 +1,9 @@
+
+
+const QueryInput = () => {
+  return (
+    <input type="text" placeholder="Type here" className="input input-bordered w-full max-w-xs" />
+  )
+}
+
+export default QueryInput
